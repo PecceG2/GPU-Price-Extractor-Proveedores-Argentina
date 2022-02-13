8fb95bb65fc86dd83dc7066ae6fd57e44596d375
PecceG2 <peccettog@live.com>
2022-02-12 23:34:26 -0300

    + Agregar maximus + Agregar más marcas * Correcciones menores

--- generate_gpu_list.py
+++ generate_gpu_list.py
@@ -5,6 +5,7 @@ import json
 import pandas as pd
 import re
 import time
+from datetime import datetime
 
 
 # Config vars
@@ -13,6 +14,7 @@ websiteCG1 = 'https://compragamer.com/?seccion=3&cate=6'
 websiteCG2 = 'https://compragamer.com/?seccion=3&cate=62'
 websiteFullH4rd = 'https://www.fullh4rd.com.ar/cat/supra/3/placas-de-video/'
 websiteLOGG = 'https://www.logg.com.ar/PRODUCTOS/PLACA-DE-VIDEO'
+websiteMaximus = 'https://www.maximus.com.ar/Productos/Placas-de-Video/CAT=48/OR=1/maximus.aspx'
 driverPath = './chromedriver.exe'
 
 # Placas list
@@ -36,12 +38,18 @@ def getChipBrand(productName):
         
     
 def getBrand(productName):
-    brands = ["palit", "msi", "gigabyte", "powercolor", "sapphire", "afox", "asrock", "pny", "gainward", "evga", "zotac", "xfx", "arktek", "asus", "coolermaster", "inno3d"]
+    brands = ["palit", "msi", "gigabyte", "powercolor", "sapphire", "afox", "asrock", "pny", "gainward", "evga", "zotac", "xfx", "arktek", "asus", "coolermaster", "inno3d", "aorus", "colorful", "tul corporation", "club3d", "vtx3d", "kfa2", "oem", "yeston", "asl", "onda", "veinida", "maxsun"]
     if any(ext in productName.lower() for ext in brands):
-        return [ext for ext in brands if(ext in productName.lower())][0]
+        return [ext for ext in brands if(ext in productName.lower())][0].upper()
+    else:
+        return "OTHER"
     
     
 def isLHR(productName):
+    noLHRFrases = ["Sin LHR", "No LHR", "No tiene LHR"]
+    if any(element.upper() in productName.upper() for element in noLHRFrases):
+        return "NO LHR"
+
     if "LHR" in productName:
         return "LHR"
     else:
@@ -55,6 +63,14 @@ def closeWebDriver(driver):
     driver.quit()
 
 def addGPU(proveedor, chipBrand, productBrand, productName, LHR, productPrice, productURL, productImageURL):
+    # Quitar productos mezclados de Coolermaster
+    if "coolermaster" in productName.lower():
+        return
+
+    # Intentar obtener sólo el modelo o datos técnicos (y no repetidos de otras columnas)
+    stringsARemover = {"placa de video", "video", "gpu", chipBrand, productBrand, "sin lhr", "con lhr", "lhr", "oferta", "remate", "nvidia", "geforce", "rtx", "gt7", "gtx", "quadro", "amd", "radeon", "intel", "arc", "alchemist"}
+    productName  = ' '.join([word for word in productName.lower().split() if word.lower() not in stringsARemover]) 
+
     individualGPU = []
     individualGPU.append(proveedor)
     individualGPU.append(chipBrand)
@@ -80,7 +96,6 @@ def getVENEX(webURL):
     driver.get(webURL)
     productos = driver.find_elements(By.CLASS_NAME, 'product-box')
 
-    productosFullList = []
     for producto in productos:
         # Selectors & parsers
         productSelector = producto.find_element(By.CSS_SELECTOR, '.product-box-title a')
@@ -101,7 +116,6 @@ def getVENEX(webURL):
         addGPU("VENEX", getChipBrand(productName), productBrand, productName, isLHR(productName), productPrice, productURL, productImageURL)
    
     closeWebDriver(driver)
-    return productosFullList;
         
 def getCG(webURL):
 
@@ -111,7 +125,6 @@ def getCG(webURL):
     time.sleep(6)
     productos = driver.find_elements(By.CLASS_NAME, 'card-product')
 
-    productosFullList = []
     for producto in productos:
     
         # Selectors & parsers
@@ -122,10 +135,8 @@ def getCG(webURL):
             
         # Add
         addGPU("CompraGamer", getChipBrand(productName), getBrand(productName), productName, isLHR(productName), productPrice, productURL, productImageURL)
-        
-  
+
     closeWebDriver(driver)
-    return productosFullList;
 
 def getFullH4rd(webURL):
     
@@ -143,7 +154,6 @@ def getFullH4rd(webURL):
         else:
             pasoPaginacionActual = pasoPaginacionActual+200
         
-        productosFullList = []
         for producto in productos:
         
             # Selectors & parsers
@@ -153,7 +163,7 @@ def getFullH4rd(webURL):
             productImageURL = producto.find_element(By.CSS_SELECTOR, '.image img').get_attribute('src')
                 
             # Add
-            addGPU("FULLH4RD", getChipBrand(productName), getBrand(productName), productName, isLHR(productName), productPrice, productURL, productImageURL)
+            addGPU("FullH4rd", getChipBrand(productName), getBrand(productName), productName, isLHR(productName), productPrice, productURL, productImageURL)
             
         closeWebDriver(driver)
 
@@ -180,6 +190,44 @@ def getLOGG(webURL):
     closeWebDriver(driver)
     return productosFullList;
 
+def getMaximus(webURL):
+    
+    driver = getWebDriver()
+    driver.get(webURL)
+    time.sleep(1)
+    driver.execute_script("function pageScroll() { window.scrollBy(0,40); scrolldelay = setTimeout(pageScroll,10); } pageScroll()") # Scroll al fondo para evitar el lazy loading en primera carga
+    
+    exitFromPaginationUpdate = False
+    
+    while(exitFromPaginationUpdate == False):
+        
+        productos = driver.find_elements(By.CLASS_NAME, 'product')
+        for producto in productos:
+            # Selectors & parsers
+            productName = producto.find_element(By.CSS_SELECTOR, '.description h4 a').get_attribute('innerHTML')
+            productURL = producto.find_element(By.CSS_SELECTOR, '.description h4 a').get_attribute('href')
+            productPrice = producto.find_element(By.CSS_SELECTOR, '.cajaprecio .price').text
+            productPrice =  productPrice[:int(len(productPrice))-2] + "," + productPrice[int(len(productPrice))-2:]
+            productImageURL = producto.find_element(By.CSS_SELECTOR, '.image a div img').get_attribute('src')
+                
+            # Add
+            addGPU("Maximus", getChipBrand(productName), getBrand(productName), productName, isLHR(productName), productPrice, productURL, productImageURL)
+            
+   
+        # Check if existe siguiente paso de paginación
+        paginationElements = driver.find_elements(By.CSS_SELECTOR, '.button-pager li')
+        nextPaginationButton = driver.find_element(By.CSS_SELECTOR, '.button-pager li:nth-last-child(2)')
+        
+        if len(paginationElements) <= 5:
+            # Última página escaneada. Y eso es todo amigos.
+            exitFromPaginationUpdate = True
+        else:
+            # Faltan páginas, continuar paginación
+            nextPaginationButton.click()
+            time.sleep(3) # Esperar un poco a que el browser procese el scroll JS de Maximux y nuestro scroll
+            
+            
+    closeWebDriver(driver)
 
 
 # Run getters
@@ -188,10 +236,13 @@ getCG(websiteCG1) #NVIDIA
 getCG(websiteCG2) #AMD
 getFullH4rd(websiteFullH4rd)
 getLOGG(websiteLOGG)
+getMaximus(websiteMaximus)
+
 
 # Export to CSV
+now = datetime.now()
 df = pd.DataFrame(listadoGPUs)
 print(df)
-df.to_csv('placas.csv', index=False)
+df.to_csv('placas_export'+now.strftime("_%d-%m-%Y_%H%M%S")+'.csv', index=False)
 
 
